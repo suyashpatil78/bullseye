302b2eb94f9a1d9f838f9fbb8925e84e683e57fd
suyashpatil78 <suyash.p@fyle.in>
2024-06-30 22:39:24 +0530

    feat: added styling and restart functionality to app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,9 +18,16 @@ class MainActivity : AppCompatActivity() {
 
     private var targetValue = Random.nextInt(1, 100)
 
+    private var totalScore = 0
+
+    private var totalRounds = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        supportActionBar?.hide()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -30,33 +37,52 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+
+
         setTargetValue(targetValue)
         setSeekbarListener()
-        setButtonListener(targetValue)
+        setButtonListener()
+        setStartOverButtonListener()
     }
 
     private fun setTargetValue(targetValue: Int) {
         binding.targetValue.text = targetValue.toString()
     }
 
-    private fun setButtonListener(targetValue: Int) {
-        var buttonInstance = binding.submit
+    private fun setButtonListener() {
+        val buttonInstance = binding.submit
         buttonInstance.setOnClickListener {
-            showResult(targetValue)
+            showResult()
+            setTotalScore()
+            setRounds()
         }
     }
 
-    private fun showResult(targetValue: Int) {
+    private fun showResult() {
         val resultTitle = getString(R.string.result_title)
 
         val resultMessage = setToastMessage()
 
-        val alertBuilder = AlertDialog.Builder(this)
+        AlertDialog.Builder(this)
             .setTitle(resultTitle)
             .setMessage(resultMessage)
-            .setPositiveButton("OK") {dialog, _ -> dialog.dismiss()}
+            .setPositiveButton("OK") {dialog, _ ->
+                targetValue = Random.nextInt(1, 100)
+                setTargetValue(targetValue)
+                dialog.dismiss()
+            }
             .create()
             .show()
+    }
+
+    private fun setTotalScore() {
+        totalScore += 100 - abs(sliderValue - targetValue)
+        binding.score?.text = totalScore.toString()
+    }
+
+    private fun setRounds() {
+        totalRounds += 1
+        binding.round?.text = totalRounds.toString()
     }
 
     private fun setSeekbarListener() {
@@ -81,5 +107,18 @@ class MainActivity : AppCompatActivity() {
                 "Target value: $targetValue \n" +
                 "Difference: ${abs(sliderValue - targetValue)}\n"
                 )
+    }
+
+    private fun setStartOverButtonListener() {
+        val buttonInstance = binding.startOver
+        val scoreTextInstance = binding.score
+        val roundTextInstance = binding.round
+
+        buttonInstance?.setOnClickListener {
+            totalScore = 0
+            totalRounds = 0
+            scoreTextInstance?.text = totalScore.toString()
+            roundTextInstance?.text = totalRounds.toString()
+        }
     }
 }
